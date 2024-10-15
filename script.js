f79b04b565bcf146892c67cfab7ef402bcde7c2d
aayush45123 <aayushbharda999@gmail.com>
2024-10-15 10:08:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const players = [
-  { name: "amreliya moin", image: "images/aidsa.jpeg" },
-  { name: "shubham oza", image: "images/aidsb.jpeg" },
-  // ...
-  { name: "prarambh nandusekar", image: "images/it.jpeg" }
+  { name: "Amreliya Moin", image: "images/moin.jpeg" },
+  { name: "Aayush Bharda", image: "images/aayush.jpeg" },
+  { name: "Vedant Ambavne", image: "images/vedant.jpeg" },
+  { name: "Prathamesh Chauhan", image: "images/prathamesh.jpeg" },
+  { name: "Hamza Kapasi", image: "images/hamza.jpeg" },
+  { name: "Krish Darji", image: "images/krish.jpeg" },
+  { name: "Rohan Chiplunkar", image: "images/rohan.jpeg" },
+  { name: "Vikas Chawla", image: "images/vikass.jpeg" },
+  { name: "Devansh Jadhav", image: "images/devansh.jpeg" },
+  { name: "Soham Amrule", image: "images/soham.jpeg" }
 ];
 
 const playerList = document.querySelector(".player-list");
@@ -14,22 +20,24 @@ players.forEach((player) => {
   const playerImage = document.createElement("img");
   playerImage.src = player.image;
   playerImage.alt = `${player.name} Profile Picture`;
+  playerImage.className = "player-image";
 
-  const playerName = document.createElement("a");
+  const playerName = document.createElement("div");
+  playerName.className = "player-name";
   playerName.textContent = player.name;
-  playerName.href = `playerDetail.html?player=${player.name}`;
+
+  const playerPopup = document.createElement("div");
+  playerPopup.className = "player-popup";
+
+  const playerLink = document.createElement("a");
+  playerLink.textContent = "View Profile";
+  playerLink.href = `playerDetail.html?player=${player.name}`;
+
+  playerPopup.appendChild(playerLink);
 
   playerItem.appendChild(playerImage);
   playerItem.appendChild(playerName);
+  playerItem.appendChild(playerPopup);
 
   playerList.appendChild(playerItem);
-});
-
-// Handle click events on player items
-playerList.addEventListener("click", (e) => {
-  if (e.target.tagName === "A") {
-    const playerName = e.target.textContent;
-    // Redirect to player detail page with the selected player's name
-    window.location.href = `playerDetail.html?player=${playerName}`;
-  }
 });
